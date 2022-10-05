31a61ab30f4940cb38efb5888a8bf58ac693536f
onursmlv <onurtis@code.edu.az>
2022-10-05 22:59:46 +0400

    Slider Added"
    .

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import Category from "../components/Category/category";
 import Favorites from "../components/Favorites/Favorites";
+import Slider from "../components/Slider/HomeSlider"
 function Home() {
   return (
     <div>
       <Category/>
+      <Slider/>
       <Favorites/>
     </div>
   );
